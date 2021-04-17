16b71e0a68e9f1da01fb45f821b305fe88085bc7
BestCodrEver <70841501+BestCodrEver@users.noreply.github.com>
2021-04-17 12:03:04 +1000

    FormAPI FTW

--- src/BestCodrEver/ICS/EventListener.php
+++ src/BestCodrEver/ICS/EventListener.php
@@ -6,7 +6,8 @@ use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
-
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use jojoe77777\FormAPI\CustomForm;
 use jojoe77777\FormAPI\Form;
 
@@ -19,4 +20,26 @@ class EventListener implements Listener {
     $this->plugin = $plugin;
     $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
   }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+  switch($cmd->getName()) {
+    case "ireport":
+      if(!$sender instanceof Player) {
+        $sender->sendMessage("This command must be run ingame.");
+      } else { 
+        //Form
+        $guide = new CustomForm(function (Player $player, $data = null){
+        if ($data === null){
+           return true;
+        }
+          $guide->setTitle("Instant Player Report");
+          $guide->setDescription("");
+        }
+      }
+    break;
+  }
+  return true;
+}
+  
+  
 }
