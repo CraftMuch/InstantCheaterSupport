dd83a9dd028a65084c5c0eec867800611dd635f6
BestCodrEver <70841501+BestCodrEver@users.noreply.github.com>
2021-04-17 11:51:29 +1000

    Update Main.php

--- src/BestCodrEver/ICS/Main.php
+++ src/BestCodrEver/ICS/Main.php
@@ -8,5 +8,15 @@ use pocketmine\Player;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 use pocketmine\command\Command;
+use BestCodrEver\ICS\EventListener;
 use jojoe77777\FormAPI\Form;
 use jojoe77777\FormAPI\CustomForm;
+
+class Main extends PluginBase{
+  
+  public function onEnable(){
+		    $this->eventListener = new EventListener($this);
+    		$this->getLogger()->info("System Enabled");
+  }
+
+}
